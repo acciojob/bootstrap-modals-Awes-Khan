d08aff4685f9314ef931a61a882b0a7677b342ca
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-07-25 01:11:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document.getElementById('open-modal').addEventListener('click', function () {
     const modal = document.getElementById('newton-modal');
     modal.classList.toggle('show');
 });
+document.getElementById('close-modal').addEventListener('click', function () {
+    const modal = document.getElementById('newton-modal');
+    modal.classList.toggle('show');
+});
