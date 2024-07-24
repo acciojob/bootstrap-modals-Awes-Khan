06af1f1b33a90d1b2542eff8ee674d2e3d923a04
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-07-25 01:06:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your code here
+document.getElementById('open-modal').addEventListener('click', function () {
+    const modal = document.getElementById('newton-modal');
+    modal.classList.toggle('show');
+});
